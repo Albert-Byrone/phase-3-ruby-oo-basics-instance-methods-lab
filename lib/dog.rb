956d90c70305b25239aed1d34b2e1a77651d4223
Albert Byrone <albertbyrone1677@gmail.com>
2022-05-30 15:03:28 +0300

    commit tree 03a1946a87bc82bcf844d25ede812767052e1904

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+class Dog
+    # Class body
+  
+    # Instance Method Definition
+    def bark
+      puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+
+class Person
+    # Class body
+  
+    # Instance Method Definition
+    def talk
+      puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+
